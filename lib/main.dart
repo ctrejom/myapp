fcab3a9a99cb552433c0a63fdcd0908dc9c640a4
trejomejia13 <trejomejia13@gmail.com>
2024-07-17 20:46:52 +0000

    Programa Inicial

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tercera Tarea',
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2978350538.
+      debugShowCheckedModeBanner: false,
+    )
+  }
+}
+
