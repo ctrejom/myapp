340777bb69b98e4496e1773f8b2d0236dfc37437
trejomejia13 <trejomejia13@gmail.com>
2024-07-18 05:43:29 +0000

    prueba

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:myapp/login_page.dart';
 import 'package:myapp/register_page.dart';
 import 'package:myapp/inicio_page.dart';
-
+//bjbhbh
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+// xdd
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
