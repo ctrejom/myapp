09579487dff2e2a9e2987e07ab89a709adc62e55
trejomejia13 <trejomejia13@gmail.com>
2024-07-18 00:31:37 +0000

    Poner las rutas de la app en el main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tercera Tarea',
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:2978350538.
       debugShowCheckedModeBanner: false,
-    )
+      title: 'Tercera Tarea',
+      initialRoute: 'login', //Ruta previamente definida
+      routes: {
+        //'login': (context) => LoginPage(),
+        //'Registrar': (context) => RegisterPage(),
+        //'inicio': (context) => const InicioPage(),
+      },
+    );
   }
 }
-
